1afa34c4878552199326ffd25e32731705fe81bd
Emin Senay <eminsenay@gmail.com>
2012-03-25 14:31:27 +0200

    Forgotten file commit for Datagrid validation check methods

--- IremEczOtomasyonu/Utilities.cs
+++ IremEczOtomasyonu/Utilities.cs
@@ -26,5 +26,33 @@ namespace IremEczOtomasyonu
                 ((Calendar)popup.Child).DisplayMode = CalendarMode.Decade;
             }
         }
+
+        public static bool HasDataGridErrors(DataGrid dataGrid)
+        {
+            bool hasDataGridErrors = false;
+            for (int i = 0; i < dataGrid.Items.Count; i++)
+            {
+                DataGridRow row = GetRow(dataGrid, i);
+                if (row != null && Validation.GetHasError(row))
+                {
+                    hasDataGridErrors = true;
+                    break;
+                }
+            }
+            return hasDataGridErrors;
+        }
+
+        private static DataGridRow GetRow(DataGrid dataGrid, int index)
+        {
+            DataGridRow row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromIndex(index);
+            if (row == null)
+            {
+                // May be virtualized, bring into view and try again.
+                dataGrid.UpdateLayout();
+                dataGrid.ScrollIntoView(dataGrid.Items[index]);
+                row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromIndex(index);
+            }
+            return row;
+        }
     }
 }
