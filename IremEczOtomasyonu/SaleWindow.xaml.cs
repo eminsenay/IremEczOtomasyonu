75a1aa2777a1c1ab1934f03947d57cc4dd668b21
Emin Senay <eminsenay@gmail.com>
2012-04-01 01:23:13 +0200

    refactoring --> most of the salelist window controls are moved to a user control

--- IremEczOtomasyonu/SaleWindow.xaml.cs
+++ IremEczOtomasyonu/SaleWindow.xaml.cs
@@ -15,10 +15,6 @@ namespace IremEczOtomasyonu
     public partial class SaleWindow : Window
     {
         private readonly Model1Container _dbContext;
-        
-        private bool _isManualEditCommit;
-
-        public ProductSale CurrentProductSale { get; private set; }
 
         /// <summary>
         /// Initializes a new sale window
@@ -28,18 +24,15 @@ namespace IremEczOtomasyonu
         {
             InitializeComponent();
             _dbContext = dbContext;
+            
+            userControlSales.DbContext = dbContext;
+            userControlSales.CurrentProductSale = new ProductSale
+                                                  {
+                                                      SaleItems = new EntityCollection<SaleItem>(),
+                                                      SaleDate = DateTime.Now,
+                                                      TotalPrice = 0
+                                                  };
 
-            CurrentProductSale = new ProductSale
-                                 {
-                                     SaleItems = new EntityCollection<SaleItem>(),
-                                     SaleDate = DateTime.Now, 
-                                     TotalPrice = 0
-                                 };
-
-            saleGrid.DataContext = CurrentProductSale;
-            productSaleDataGrid.ItemsSource = CurrentProductSale.SaleItems;
-
-            UpdateTotalPriceTextBox();
         }
 
         /// <summary>
@@ -49,131 +42,31 @@ namespace IremEczOtomasyonu
         /// <param name="customer">Customer who is buying the products</param>
         public SaleWindow(Model1Container dbContext, Customer customer): this(dbContext)
         {
-            CurrentProductSale.Customer = customer;
+            userControlSales.CurrentProductSale.Customer = customer;
         }
 
-        /// <summary>
-        /// Adds a new product to the purchase. If the barcode cannot be found in the database, an error is raised.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ProductAddButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (barcodeTextBox == null || string.IsNullOrEmpty(barcodeTextBox.Text))
-            {
-                return;
-            }
-            Product newProduct = _dbContext.Products.FirstOrDefault(p => p.Barcode == barcodeTextBox.Text);
-            if (newProduct == null)
-            {
-                MessageBox.Show("Girmiş olduğunuz barkod sistemde kayıtlı değil.", "Ürün uyarısı", MessageBoxButton.OK,
-                                MessageBoxImage.Error);
-                barcodeTextBox.SelectAll();
-                return;
-            }
-            //SaleItems.Add(new SaleItem {Product = newProduct, NumSold = 1, ProductSale = CurrentProductSale});
-            CurrentProductSale.SaleItems.Add(new SaleItem
-                                             {
-                                                 Product = newProduct,
-                                                 NumSold = 1,
-                                                 ProductSale = CurrentProductSale,
-                                                 ExpDate = newProduct.ExpirationDates.FirstOrDefault(),
-                                                 UnitPrice = newProduct.CurrentSellingPrice ?? 0
-                                             });
-            UpdateTotalPriceTextBox();
-
-            barcodeTextBox.Text = string.Empty;
-            barcodeTextBox.Focus();
-        }
-
-        /// <summary>
-        /// CellEditEnding event handler of the datagrid. Commits the change and updates the total price textbox.
-        /// Manual change committing is required to update the saleitem price if the user presses for ex. tab button
-        /// instead of return.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ProductSaleDataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            if (_isManualEditCommit)
-            {
-                return;
-            }
-            _isManualEditCommit = true;
-            productSaleDataGrid.CommitEdit(DataGridEditingUnit.Row, true);
-            _isManualEditCommit = false;
-            UpdateTotalPriceTextBox();
-        }
-
-        /// <summary>
-        /// Manual handle for the Delete key event.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ProductSaleDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Delete)
-            {
-                DeleteSelectedSaleItem();
-                e.Handled = true;
-            }
-        }
-
-        /// <summary>
-        /// Event handler for the delete context menu item.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void DatagridDeleteSaleItemMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            DeleteSelectedSaleItem();
-        }
-
-        /// <summary>
-        /// Deletes the selected sale item from the datagrid without any questions.
-        /// </summary>
-        private void DeleteSelectedSaleItem()
-        {
-            SaleItem saleItem = productSaleDataGrid.SelectedItem as SaleItem;
-            if (saleItem == null)
-            {
-                return;
-            }
-
-            CurrentProductSale.SaleItems.Remove(saleItem);
-            UpdateTotalPriceTextBox();
-        }
-
-        /// <summary>
-        /// Updates the total price text block with the sum of the purchases.
-        /// </summary>
-        private void UpdateTotalPriceTextBox()
-        {
-            decimal totalPrice = CurrentProductSale.SaleItems.Sum(x => (x.NumSold * x.UnitPrice));
-            totalPriceTextBox.Text = totalPrice.ToString(CultureInfo.CurrentCulture);
-            //CurrentProductSale.TotalPrice = totalPrice;
-        }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
             // validation check
-            if (Validation.GetHasError(totalPriceTextBox) || UIUtilities.HasDataGridErrors(productSaleDataGrid))
+            string validationMsg = userControlSales.Validate();
+            if (validationMsg != null)
             {
-                MessageBox.Show("Girdiğiniz bazı bilgiler eksik ya da hatalı. \n Lütfen düzeltip tekrar deneyin.",
-                                "Satış uyarısı", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(validationMsg, "Satış uyarısı", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
-            if (CurrentProductSale.SaleItems.Count == 0)
+            
+            if (userControlSales.CurrentProductSale.SaleItems.Count == 0)
             {
                 DialogResult = true;
                 Close();
                 return;
             }
 
-            CurrentProductSale.Id = Guid.NewGuid();
+            userControlSales.CurrentProductSale.Id = Guid.NewGuid();
 
             // Set entity expiration dates and ids manually
-            foreach (SaleItem saleItem in CurrentProductSale.SaleItems)
+            foreach (SaleItem saleItem in userControlSales.CurrentProductSale.SaleItems)
             {
                 saleItem.ExDate = saleItem.ExpDate.ExDate;
                 // Decrease the item count from products & expiration date tables
@@ -182,25 +75,10 @@ namespace IremEczOtomasyonu
                 saleItem.Id = Guid.NewGuid();
             }
             
-            _dbContext.AddToProductSales(CurrentProductSale);
+            _dbContext.AddToProductSales(userControlSales.CurrentProductSale);
             _dbContext.SaveChanges();
             DialogResult = true;
             Close();
-        }
-
-        private void CustomerFindButton_Click(object sender, RoutedEventArgs e)
-        {
-            CustomerListWindow customerListWindow = new CustomerListWindow(_dbContext)
-                                                    {
-                                                        SelectedCustomer = CurrentProductSale.Customer,
-                                                        Owner = this,
-                                                        WindowStartupLocation = WindowStartupLocation.CenterOwner
-                                                    };
-            customerListWindow.ShowDialog();
-            if (customerListWindow.DialogResult == true)
-            {
-                CurrentProductSale.Customer = customerListWindow.SelectedCustomer;
-            }
         }
     }
 }
---
+++ IremEczOtomasyonu/UserControlSales.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects.DataClasses;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IremEczOtomasyonu.BL;
+
+namespace IremEczOtomasyonu
+{
+    /// <summary>
+    /// Interaction logic for UserControlSales.xaml
+    /// </summary>
+    public partial class UserControlSales : UserControl
+    {
+        private bool _isManualEditCommit;
+        private ProductSale _currentProductSale;
+        private Model1Container _dbContext;
+
+        public Model1Container DbContext
+        {
+            get { return _dbContext ?? (_dbContext = new Model1Container()); }
+            set { _dbContext = value; }
+        }
+        public ProductSale CurrentProductSale
+        {
+            get { return _currentProductSale; }
+            set 
+            { 
+                _currentProductSale = value;
+                saleGrid.DataContext = CurrentProductSale;
+                productSaleDataGrid.ItemsSource = CurrentProductSale.SaleItems;
+                UpdateTotalPriceTextBox();
+            }
+        }
+
+        public UserControlSales()
+        {
+            InitializeComponent();
+        }
+
+        public UserControlSales(Model1Container dbContext, ProductSale productSale)
+        {
+            InitializeComponent();
+
+            DbContext = dbContext;
+            CurrentProductSale = productSale;
+        }
+
+        /// <summary>
+        /// Adds a new product to the purchase. If the barcode cannot be found in the database, an error is raised.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProductAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (barcodeTextBox == null || string.IsNullOrEmpty(barcodeTextBox.Text))
+            {
+                return;
+            }
+            Product newProduct = DbContext.Products.FirstOrDefault(p => p.Barcode == barcodeTextBox.Text);
+            if (newProduct == null)
+            {
+                MessageBox.Show("Girmiş olduğunuz barkod sistemde kayıtlı değil.", "Ürün uyarısı", MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                barcodeTextBox.SelectAll();
+                return;
+            }
+            CurrentProductSale.SaleItems.Add(new SaleItem
+            {
+                Product = newProduct,
+                NumSold = 1,
+                ProductSale = CurrentProductSale,
+                ExpDate = newProduct.ExpirationDates.FirstOrDefault(),
+                UnitPrice = newProduct.CurrentSellingPrice ?? 0
+            });
+            UpdateTotalPriceTextBox();
+
+            barcodeTextBox.Text = string.Empty;
+            barcodeTextBox.Focus();
+        }
+
+        /// <summary>
+        /// CellEditEnding event handler of the datagrid. Commits the change and updates the total price textbox.
+        /// Manual change committing is required to update the saleitem price if the user presses for ex. tab button
+        /// instead of return.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProductSaleDataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (_isManualEditCommit)
+            {
+                return;
+            }
+            _isManualEditCommit = true;
+            productSaleDataGrid.CommitEdit(DataGridEditingUnit.Row, true);
+            _isManualEditCommit = false;
+            UpdateTotalPriceTextBox();
+        }
+
+        /// <summary>
+        /// Manual handle for the Delete key event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProductSaleDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                DeleteSelectedSaleItem();
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the delete context menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DatagridDeleteSaleItemMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteSelectedSaleItem();
+        }
+
+        /// <summary>
+        /// Deletes the selected sale item from the datagrid without any questions.
+        /// </summary>
+        private void DeleteSelectedSaleItem()
+        {
+            SaleItem saleItem = productSaleDataGrid.SelectedItem as SaleItem;
+            if (saleItem == null)
+            {
+                return;
+            }
+
+            CurrentProductSale.SaleItems.Remove(saleItem);
+            UpdateTotalPriceTextBox();
+        }
+
+        /// <summary>
+        /// Updates the total price text block with the sum of the purchases.
+        /// </summary>
+        private void UpdateTotalPriceTextBox()
+        {
+            decimal totalPrice = CurrentProductSale.SaleItems.Sum(x => (x.NumSold * x.UnitPrice));
+            totalPriceTextBox.Text = totalPrice.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void CustomerFindButton_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerListWindow customerListWindow = new CustomerListWindow(DbContext)
+            {
+                SelectedCustomer = CurrentProductSale.Customer,
+                Owner = Parent as Window,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+            customerListWindow.ShowDialog();
+            if (customerListWindow.DialogResult == true)
+            {
+                CurrentProductSale.Customer = customerListWindow.SelectedCustomer;
+            }
+        }
+
+        public string Validate()
+        {
+            if (Validation.GetHasError(totalPriceTextBox) || UIUtilities.HasDataGridErrors(productSaleDataGrid))
+            {
+                return "Girdiğiniz bazı bilgiler eksik ya da hatalı. \n Lütfen düzeltip tekrar deneyin.";
+            }
+            return null;
+        }
+    }
+}
