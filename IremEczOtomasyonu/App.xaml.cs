8a30e482203e49d6ec7704270d6a671e52d3c5db
Emin Senay <eminsenay@gmail.com>
2012-07-25 14:26:59 +0300

    Global exception catch mechanism

--- IremEczOtomasyonu/App.xaml.cs
+++ IremEczOtomasyonu/App.xaml.cs
@@ -12,5 +12,13 @@ namespace IremEczOtomasyonu
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Uncaught Exception",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(e.Exception.ToString(), "Uncaught Exception",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
