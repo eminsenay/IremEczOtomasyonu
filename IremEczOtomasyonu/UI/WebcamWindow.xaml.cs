949425f090838eb188f14c171450d5be50914767
Emin Senay <eminsenay@gmail.com>
2019-10-19 23:51:26 +0300

    Started using OpenCvSharp4 for webcam capturing. Getting the capturing interface is hardcoded for now.

--- IremEczOtomasyonu/UI/WebcamWindow.xaml.cs
+++ IremEczOtomasyonu/UI/WebcamWindow.xaml.cs
@@ -5,24 +5,30 @@ using System.Windows.Controls;
 using System.Windows.Forms;
 using Touchless.Vision.Camera;
 using MessageBox = System.Windows.MessageBox;
+using OpenCvSharp;
+using System.Threading;
+using OpenCvSharp.Extensions;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
 
 namespace IremEczOtomasyonu.UI
 {
     /// <summary>
     /// Interaction logic for WebcamWindow.xaml
     /// </summary>
-    public partial class WebcamWindow : Window
+    public partial class WebcamWindow
     {
         public Bitmap GrabbedImage { get; private set; }
-        private static Bitmap _latestFrame;
-        private CameraFrameSource _frameSource;
-        private readonly PictureBox _pictureBoxDisplay;
+        private VideoCapture _videoCapture;
+        private Thread _cameraThread;
+        private bool _isCameraRunning = true;
+
 
         public WebcamWindow()
         {
             InitializeComponent();
-            _pictureBoxDisplay = new PictureBox();
-            host.Child = _pictureBoxDisplay;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -44,14 +50,14 @@ namespace IremEczOtomasyonu.UI
         private void WebcamComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // Display a preview
-            ThrashOldCamera();
+            StopCapturing();
             StartCapturing();
             SaveSelectedWebcam();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            ThrashOldCamera();
+            StopCapturing();
         }
 
         /// <summary>
@@ -70,13 +76,13 @@ namespace IremEczOtomasyonu.UI
 
         private void GrabImageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_frameSource == null)
+            if (!_isCameraRunning)
             {
                 return;
             }
-
             // Clone the image to use it in preview window
-            GrabbedImage = (Bitmap) _latestFrame.Clone();
+            GrabbedImage = ConvertBitmapSource2Bitmap((BitmapSource)pictureBox.Source);
+
             WebcamImagePreviewWindow previewWindow = new WebcamImagePreviewWindow(GrabbedImage) { Owner = this };
             bool? dialogResult = previewWindow.ShowDialog();
             // If the user accepts to use the image, close the window
@@ -92,61 +98,58 @@ namespace IremEczOtomasyonu.UI
         /// </summary>
         private void StartCapturing()
         {
-            try
-            {
-                Camera c = (Camera)webcamComboBox.SelectedItem;
-                SetFrameSource(new CameraFrameSource(c));
-                _frameSource.Camera.CaptureWidth = 320;
-                _frameSource.Camera.CaptureHeight = 240;
-                _frameSource.Camera.Fps = 20;
-                _frameSource.NewFrame += OnImageCaptured;
-
-                _pictureBoxDisplay.Paint += DrawLatestImage;
-                _frameSource.StartFrameCapture();
-            }
-            catch (Exception ex)
-            {
-                webcamComboBox.Text = "Bir Webcam Seçiniz";
-                MessageBox.Show(ex.Message);
-            }
+            _cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+            _isCameraRunning = true;
+            _cameraThread.Start();
         }
 
-        public void OnImageCaptured(Touchless.Vision.Contracts.IFrameSource frameSource, 
-            Touchless.Vision.Contracts.Frame frame, double fps)
+        private void CaptureCameraCallback()
         {
-            _latestFrame = frame.Image;
-            _pictureBoxDisplay.Invalidate();
-        }
+            Mat frame = new Mat();
+            _videoCapture = new VideoCapture(0);
+            _videoCapture.Open(0); // TODO: Change the index based on webcam selection
 
-        private void DrawLatestImage(object sender, PaintEventArgs e)
-        {
-            if (_latestFrame != null)
-            {
-                // Draw the latest image from the active camera
-                e.Graphics.DrawImage(_latestFrame, 0, 0, _latestFrame.Width, _latestFrame.Height);
-            }
-        }
-
-        private void ThrashOldCamera()
-        {
-            // Trash the old camera
-            if (_frameSource != null)
-            {
-                _frameSource.NewFrame -= OnImageCaptured;
-                _frameSource.Camera.Dispose();
-                SetFrameSource(null);
-                _pictureBoxDisplay.Paint -= DrawLatestImage;
-            }
-        }
-
-        private void SetFrameSource(CameraFrameSource cameraFrameSource)
-        {
-            if (cameraFrameSource != null && _frameSource == cameraFrameSource)
+            if (!_videoCapture.IsOpened())
             {
                 return;
             }
 
-            _frameSource = cameraFrameSource;
+            while (_isCameraRunning)
+            {
+                _videoCapture.Read(frame);
+                Bitmap image = BitmapConverter.ToBitmap(frame);
+                Dispatcher.Invoke(() => pictureBox.Source = ConvertBitmap2BitmapImage(image));
+                image.Dispose();
+            }
         }
+
+        private BitmapImage ConvertBitmap2BitmapImage(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            src.Save(ms, ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        public static Bitmap ConvertBitmapSource2Bitmap(BitmapSource bitmapSource)
+        {
+            var width = bitmapSource.PixelWidth;
+            var height = bitmapSource.PixelHeight;
+            var stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+            var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
+            bitmapSource.CopyPixels(new Int32Rect(0, 0, width, height), memoryBlockPointer, height * stride, stride);
+            var bitmap = new Bitmap(width, height, stride, PixelFormat.Format32bppPArgb, memoryBlockPointer);
+            return bitmap;
+        }
+
+        private void StopCapturing()
+        {
+            _isCameraRunning = false;
+        }
+
     }
 }
